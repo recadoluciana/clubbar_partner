c8d65f33ff638a6f3d15d79ef533fc320ecbf534
Luciana Correa <recadoluciana@gmail.com>
2026-08-12 21:34:45 -0300

    troca de nome da apk

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -100,6 +100,32 @@ android {
             isShrinkResources = false
         }
     }
+    applicationVariants.all {
+        val flavor = flavorName
+        val build = buildType.name
+        val apkName = "partner-$flavor-$build.apk"
+
+        outputs.all {
+            val output = this as com.android.build.gradle.internal.api.BaseVariantOutputImpl
+            output.outputFileName = apkName
+        }
+
+        assembleProvider.configure {
+            doLast {
+                val sourceApk = layout.buildDirectory
+                    .file("outputs/apk/$flavor/$build/$apkName")
+                    .get()
+                    .asFile
+                val flutterOutputDir = layout.buildDirectory
+                    .dir("outputs/flutter-apk")
+                    .get()
+                    .asFile
+
+                flutterOutputDir.mkdirs()
+                sourceApk.copyTo(flutterOutputDir.resolve(apkName), overwrite = true)
+            }
+        }
+    }
 }
 
 flutter {
